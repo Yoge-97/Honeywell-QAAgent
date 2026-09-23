adc24c8ed7558b588927c565113dd0a8a7ff8181
kiruthika-knack26 <kiruthika.y@knackforge.com>
2026-09-23 11:41:48 +0530

    Delete shared directory

--- shared/config.py
+++
@@ -1,9 +0,0 @@
-PROJECT_NAME = "qa-testing-agent"
-
-INPUT_DIRECTORY = "input"
-
-OUTPUT_DIRECTORY = "output"
-
-PLANNER_OUTPUT_FILE = "output/planner_output.json"
-
-KARATE_OUTPUT_DIRECTORY = "output/karate"
